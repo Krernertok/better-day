a4f73fd54416533324efbfdac37de273d1ca606f
Antti Sivula <antti.j.sivula@gmail.com>
2021-02-08 23:56:49 +0200

    Add initial frontend files

---
+++ frontend/src/js/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return <div><h1>Better Days</h1></div>
+}
+
+const root = document.getElementById('root')
+ReactDOM.render(<App />, root)
---
+++ frontend/webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: ['@babel/polyfill', './src/js/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'build'),
+    compress: true,
+    port: 3000
+  },
+  devtool: 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'html', 'index.html')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    ]
+  }
+}
